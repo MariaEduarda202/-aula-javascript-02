49f26cb71699e4dd9fc23080c350a0c42626e3ce
Maria EDuarda <mariaeduardaferrreiragomes2021@gmail.com>
2021-09-17 21:38:52 -0500

    atividade

---
+++ atividade01.js
@@ -0,0 +1,13 @@
+var nome = "Maria Eduarda"
+ 
+console.log ("Meu nome é"+ nome)
+
+let idade = 21
+
+console.log ( "Tenho "  + idade +  "anos")
+ 
+const CPF = "1234567891011" 
+ 
+console.log ( "Meu CPF é" + CPF )
+
+ 
